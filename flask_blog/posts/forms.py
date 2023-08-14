f6b38f6be313b7e6d7e771554ea42023bf7dd1bf
Cody-Gao-Python <gg1671821@gmail.com>
2023-08-14 12:03:29 +0800

    fix update bug

--- flask_blog/posts/forms.py
+++ flask_blog/posts/forms.py
@@ -12,3 +12,11 @@ class PostForm(FlaskForm):
     content = CKEditorField('Content', validators=[DataRequired()])
     
     submit = SubmitField('Post')
+
+class Update_PostForm(FlaskForm): # 新增這個form的原因是要把category這個表單元素拿掉，因為如果用含有category的表的會有一個奇怪的bug，就是在submit update post的時候過不了驗證
+    
+    title = StringField('Title', validators=[DataRequired()])
+    
+    content = CKEditorField('Content', validators=[DataRequired()])
+    
+    submit = SubmitField('Post')
